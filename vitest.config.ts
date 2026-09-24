b8391f8c3591169d22da345f2d4788611fb8ddb0
Rohit Agrawal <29514438+agrawal-rohit@users.noreply.github.com>
2026-09-24 11:48:02 +0530

    chore: small checkpoint

--- vitest.config.ts
+++ vitest.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
 				"docs/**",
 				"**/coverage/**",
 				"packages/*/dist/**",
+				"packages/cli/bin/**",
 				"**/commitlint.config.*",
 				"**/lint-staged.config.js",
 				"**/stryker.config.mjs",
